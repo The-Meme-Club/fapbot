9af649c427c9ff53b34bda1e9895e64861bf2772
James <jamesharrison90@yahoo.com>
2018-06-03 21:54:48 -0500

    Added a ban command

--- index.js
+++ index.js
@@ -34,9 +34,32 @@ bot.on("message", message => {
         message.channel.send(`${message.author}, we've all been there, there's always someone to talk to when you need it.`)
 
     } else if (message.content.startsWith(prefix + "jayden")) {
-                message.channel.send(`<@209395113485402123> get the fuck out of the movies`)
-    }
-        
+        message.channel.send(`<@209395113485402123> get the fuck out of the movies`)
+
+    } else if (message.content.startsWith(prefix + "ban")) {
+        let reason = args.slice(1).join(" ");
+            let user = message.mentions.users.first();
+            let logchannel = message.guild.channels.find("name", "fapbot-logs")
+    
+            if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("you don't have perms ban_members")
+            if (!logchannel) return message.reply("no log channel found. make a channel named `fapbot-logs`")
+            if (message.mentions.users.size < 1) return message.channel.send("no mention")
+            if (reason.length < 1) return message.channel.send("no reason")
+    
+            if (!message.guild.member(user).bannable) return message.channel.send("user not bannable")
+        message.guild.member(user).ban(reason);
+       const embed = new Discord.RichEmbed()
+        .setTitle(`User Banned`)
+        .setDescription(`User has been banned from the server.`)
+        .setColor(`#ff0000`)
+        .addField(`User Tag`, user.tag)
+        .addField(`User ID`, user.id)
+        .addField(`Reason`, reason)
+        bot.channels.get(logchannel.id).send({embed: embed})
+    
+        }
+    
+    
 });
 
 
